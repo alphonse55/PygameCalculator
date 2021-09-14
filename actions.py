b0f90eeeddeae8c4bf67fa011f2fa7167d177c5a
alphonse55 <alphonse.raynaud@icloud.com>
2021-09-14 10:17:51 +0200

    changed output when after pressing the equals sign: the result gets shown in big and depending on the next input it disappears or is used as the first number for the next operation.

--- actions.py
+++ actions.py
@@ -2,10 +2,17 @@ from solve import solve
 
 NUMBERS = '1234567890'
 OPERATORS = ["+", "-", "x", "/", "^"]
+operation_is_answer = False
 
 def number(operation, last_operation, answer, n):
     if len(operation) == 0 or operation[-1] != ")":
-        operation += str(n)
+        global operation_is_answer
+        if operation_is_answer:
+            operation = str(n)
+            last_operation += answer
+            operation_is_answer = False
+        else:
+            operation += str(n)
     return operation, last_operation, answer
 
 def constant(operation, last_operation, answer, n):
@@ -14,42 +21,54 @@ def constant(operation, last_operation, answer, n):
     return operation, last_operation, answer
 
 def canc(operation, last_operation, answer):
+    global operation_is_answer
+    operation_is_answer = False
     return "", "", answer
 
 def decimal_point(operation, last_operation, answer):
-    sign = False
     last_decimal_point = -1
+    sign_after_last_decimal_point = False
     for i in range(len(operation)):
         if operation[i] == ".":
             last_decimal_point = i
     if last_decimal_point == -1:
-        sign = True
+        sign_after_last_decimal_point = True
     else:
         for i in range(last_decimal_point, len(operation)):
             if operation[i] in OPERATORS:
-                sign = True
-    if operation != "" and operation[-1] in NUMBERS and sign:
+                sign_after_last_decimal_point = True
+    if not operation_is_answer and sign_after_last_decimal_point and operation != "" and operation[-1] in NUMBERS:
         operation += "."
     return operation, last_operation, answer
 
 def delete(operation, last_operation, answer):
-    return operation[:-1], last_operation, answer
+    if not operation_is_answer:
+        operation = operation[:-1]
+    return operation, last_operation, answer
 
 def sign(operation, last_operation, answer, sign):
     if operation != "" and operation[-1] in NUMBERS + ")":
         operation += sign
+        global operation_is_answer
+        last_operation += answer
+        operation_is_answer = False
     return operation, last_operation, answer
 
 def minus(operation, last_operation, answer):
     if len(operation) == 0 or not (operation[-1] in OPERATORS and operation[-2] in OPERATORS):
-        operation += "-"
+        operation += "-"        
+        global operation_is_answer
+        last_operation += answer
+        operation_is_answer = False
     return operation, last_operation, answer
 
 def equals(operation, last_operation, answer):
     try:
         answer = solve(operation)
-        last_operation = operation + "=" + answer
-        operation = ""
+        last_operation = operation + "="
+        operation = answer
+        global operation_is_answer
+        operation_is_answer = True
     finally:
         return operation, last_operation, answer
 
@@ -69,10 +88,7 @@ def left(operation, last_operation, answer):
 def right(operation, last_operation, answer):
     counter = 0
     for c in operation:
-        if c == "(":
-            counter += 1
-        elif c == ")":
-            counter -= 1
+        counter += 1 if c == "(" else (-1 if c == ")" else 0)
     if counter > 0 and operation[-1] not in OPERATORS + ["("]:
         operation += ")"
     return operation, last_operation, answer
